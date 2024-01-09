5ef785e26b09f72cf007692f7d2a406affe30064
FiamaTalavera <fiama.viccini@gmail.com>
2024-01-09 12:09:14 -0300

    first commit

---
+++ message-widget.js
@@ -0,0 +1,35 @@
+["https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"].forEach((u) => {
+  var script = document.createElement("script");
+  script.src = u;
+  document.head.appendChild(script);
+});
+
+class messageWidget extends HTMLElement {
+  static _WIDGETNAME = "message-widget";
+  static _WIDGETURL = "https://lively-stone-0bffa0e10.4.azurestaticapps.net";
+
+  connectedCallback() {
+    this.classList.add("neo-widget");
+    this.classList.add(`widget--${messageWidget._WIDGETNAME}`);
+    this.style.overflowX = "hidden";
+
+    fetch(`${messageWidget._WIDGETURL}/${messageWidget._WIDGETNAME}.html`)
+      .then((data) => {
+       return data.text()})
+        
+      .then((html) => {
+        this.innerHTML += `<link rel="stylesheet" href="${messageWidget._WIDGETURL}/${messageWidget._WIDGETNAME}.css">`;
+        this.innerHTML += html;
+        this.initWidgetCode();
+      });
+    {
+
+    }
+  }
+
+  initWidgetCode() {
+    console.log("Init Widget code");
+  }
+}
+
+customElements.define(messageWidget._WIDGETNAME, messageWidget);
