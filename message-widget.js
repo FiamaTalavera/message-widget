48c566df2738c3e2471e5cf1fc45748ed0e2bae4
FiamaTalavera <fiama.viccini@gmail.com>
2024-01-09 12:35:26 -0300

    changes

--- message-widget.js
+++ message-widget.js
@@ -6,7 +6,7 @@
 
 class messageWidget extends HTMLElement {
   static _WIDGETNAME = "message-widget";
-  static _WIDGETURL = "https://zealous-smoke-0a5fdc510.4.azurestaticapps.net";
+  static _WIDGETURL = "https://thankful-beach-09c743510.4.azurestaticapps.net";
 
   connectedCallback() {
     this.classList.add("neo-widget");
